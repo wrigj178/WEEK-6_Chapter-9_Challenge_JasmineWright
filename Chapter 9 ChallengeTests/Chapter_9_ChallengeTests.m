c6137bcf47cc8826366ec83a6c93945d1d5a6396
wrigj178 <jjazzymane@live.com>
2014-03-10 23:25:56 -0400

    Initial Commit

---
+++ Chapter 9 ChallengeTests/Chapter_9_ChallengeTests.m
@@ -0,0 +1,34 @@
+//
+//  Chapter_9_ChallengeTests.m
+//  Chapter 9 ChallengeTests
+//
+//  Created by Jasmine Wright on 3/10/14.
+//  Copyright (c) 2014 Jasmine Wright. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Chapter_9_ChallengeTests : XCTestCase
+
+@end
+
+@implementation Chapter_9_ChallengeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
